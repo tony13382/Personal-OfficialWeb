b91050af05d4b2d8325bb08a6da03c6e293c930e
Meow Lu <ttsmcpe@gmail.com>
2024-07-12 01:50:29 +0800

    Update scroll.js to improve button display on scroll

--- assets/script/scroll.js
+++ assets/script/scroll.js
@@ -1,14 +1,14 @@
 function toTop(){window.scrollTo({top: 0, behavior: 'smooth'});}
 // Get the button:
-let mybutton = document.getElementById("toTopBtn");
+let myToTopButton = document.getElementById("toTopBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "flex";
+    myToTopButton.style.display = "flex";
   } else {
-    mybutton.style.display = "none";
+    myToTopButton.style.display = "none";
   }
 }
