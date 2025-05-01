d864c779677eec6d814a45a22920e7d92a7a5eb0
tony13382 <ttsmcpe@gmail.com>
2025-05-01 19:42:58 +0800

    更新 Head 類別以接受 colorSet 參數，並在 JobPage 和 ProjectPage 中傳遞該參數以改善主題顏色管理；在 index.html 中新增學校資訊以增強內容完整性。

--- app/components/webHead.py
+++ app/components/webHead.py
@@ -1,3 +1,8 @@
+from typing import Tuple
+
+from app.variables import ThemeColor
+
+
 class Head:
     def __init__(
         self,
@@ -5,45 +10,88 @@ class Head:
         description: str = "",
         cover: str = "/assets/MetaTagCover.png",
         link: str = "/",
+        colorSet: Tuple = ThemeColor().natural,
     ):
         self.title = title
         self.description = description
         self.cover = cover
         self.link = link
+        self.colorSet = colorSet
 
     def __str__(self):
+        theme_color, second_color = self.colorSet
         return f"""
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta name="Description" CONTENT="{self.description}">
-            <!-- Facebook Opengraph -->
-            <meta property="og:url" content="{self.link}" />
-            <meta property="og:type" content="website" />
-            <meta property="og:title" content="{self.title}" />
-            <meta property="og:description" content="{self.description}" />
-            <meta property="og:image" content="{self.cover}" />
-            <meta property="og:image:alt" content="{self.title}" />
-            <meta property="og:image:type" content="{self.cover}" />
-            <meta property="og:image:width" content="1200px" />
-            <meta property="og:image:height" content="630px" />
-            <!-- End Facebook Opengraph -->
-            <title>{self.title}・呂亮進</title>
-            <link rel="Shortcut Icon" type="image/x-icon" href="/favicon.png" />
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Description" CONTENT="{self.description}">
+    <!-- Facebook Opengraph -->
+    <meta property="og:url" content="{self.link}" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="{self.title}" />
+    <meta property="og:description" content="{self.description}" />
+    <meta property="og:image" content="{self.cover}" />
+    <meta property="og:image:alt" content="{self.title}" />
+    <meta property="og:image:type" content="{self.cover}" />
+    <meta property="og:image:width" content="1200px" />
+    <meta property="og:image:height" content="630px" />
+    <!-- End Facebook Opengraph -->
+    <title>{self.title}・呂亮進</title>
+    <link rel="Shortcut Icon" type="image/x-icon" href="/favicon.png" />
 
-            <!-- Self CSS -->
-            <link rel="stylesheet" href="/assets/stylesheets/myself.css">
-            <link rel="stylesheet" href="/assets/stylesheets/all.css">
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-            <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-                integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-            <!-- End of Self CSS -->
-            <!-- Fancyapp (Image Box)-->
-            <!-- CSS -->
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4/dist/fancybox.css" />
-            <!-- JavaScript -->
-            <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4/dist/fancybox.umd.js"></script>
-            <!-- End of Fancyapp (Image Box)-->
-        </head>
+    <!-- Self CSS -->
+    <link rel="stylesheet" href="/assets/stylesheets/myself.css">
+    <link rel="stylesheet" href="/assets/stylesheets/all.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <!-- End of Self CSS -->
+    <!-- Fancyapp (Image Box)-->
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4/dist/fancybox.css" />
+    <!-- JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4/dist/fancybox.umd.js"></script>
+    <!-- End of Fancyapp (Image Box)-->
+    <style>
+        :root {{
+            --themeColor: {theme_color};
+            --secondColor: {second_color};
+        }}
+
+        .bg-mytheme {{
+            color: var(--secondColor);
+            position: relative;
+            letter-spacing: 0.02rem;
+            z-index: 10;
+        }}
+
+        .bg-mytheme::before {{
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: var(--themeColor);
+            opacity: 0.2;
+            z-index: -1;
+        }}
+
+        .color-mytheme {{
+            color: var(--themeColor);
+        }}
+    </style>
+    <style>
+        .carousel__nav .carousel__button {{
+            background-color: #FFFFFF;
+            box-shadow: 0 6px 12px -2px rgb(50 50 93 / 25%), 0 3px 7px -3px rgb(0 0 0 / 30%);
+        }}
+
+        .carousel__nav .carousel__button:hover {{
+            background-color: #f9f9f9;
+            box-shadow: 0 6px 12px -2px rgb(50 50 93 / 25%), 0 3px 7px -3px rgb(0 0 0 / 30%);
+        }}
+    </style>
+</head>
         """
--- app/layouts/project.py
+++ app/layouts/project.py
@@ -56,10 +56,9 @@ class ProjectPage:
                 description=self.description,
                 cover=self.cover,
                 link="/projects/" + self.prefix,
+                colorSet=self.colorSet,
             )
         )
-
-        theme_color, second_color = self.colorSet
 
         navbar_html = str(Navbar(path="project"))
 
@@ -150,50 +149,8 @@ class ProjectPage:
         return f"""
 <!doctype html>
 <html lang="zh-Hant-TW">
-
 {head_html}
-
 <body class="bg-mytheme">
-    <style>
-        :root {{
-            --themeColor: {theme_color};
-            --secondColor: {second_color};
-        }}
-
-        .bg-mytheme {{
-            color: var(--secondColor);
-            position: relative;
-            letter-spacing: 0.02rem;
-            z-index: 10;
-        }}
-
-        .bg-mytheme::before {{
-            content: "";
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background: var(--themeColor);
-            opacity: 0.2;
-            z-index: -1;
-        }}
-
-        .color-mytheme {{
-            color: var(--themeColor);
-        }}
-    </style>
-    <style>
-        .carousel__nav .carousel__button {{
-            background-color: #FFFFFF;
-            box-shadow: 0 6px 12px -2px rgb(50 50 93 / 25%), 0 3px 7px -3px rgb(0 0 0 / 30%);
-        }}
-
-        .carousel__nav .carousel__button:hover {{
-            background-color: #f9f9f9;
-            box-shadow: 0 6px 12px -2px rgb(50 50 93 / 25%), 0 3px 7px -3px rgb(0 0 0 / 30%);
-        }}
-    </style>
     {navbar_html}
     <a class="fixed-bottom text-decoration-none" onclick="toTop()" id="toTopBtn"
         style="display:none; z-index: 1000; right:16px; bottom:40px; left: auto;">
