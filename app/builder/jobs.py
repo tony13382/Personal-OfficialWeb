8e3a839d0b7bdbf97267aeb384cebc38139b524f
tony13382 <ttsmcpe@gmail.com>
2025-05-06 21:59:55 +0800

    重構 jobs.py 和 projects.py 檔案中的頁面導入方式，將頁面導入改為直接引用，提升代碼可讀性。

--- app/builder/jobs.py
+++ app/builder/jobs.py
@@ -1,21 +1,18 @@
 from app.builder.basic import WebBuilder
-from app.pages.job import (
-    y2022_Songla,
-    y2023_NthuOu,
-    y2023_NthuIr,
-    y2023_Tiic,
-    y2024_SolwenAi,
-)
+from app.pages.job.y2022_Songla import page as y2022_Songla
+from app.pages.job.y2023_NthuOu import page as y2023_NthuOu
+from app.pages.job.y2023_NthuIr import page as y2023_NthuIr
+from app.pages.job.y2023_Tiic import page as y2023_Tiic
+from app.pages.job.y2024_SolwenAi import page as y2024_SolwenAi
 
 jobBuilder = WebBuilder("jobs/")
 
-
 jobBuilder.addPages(
     [
-        y2024_SolwenAi.page,
-        y2023_Tiic.page,
-        y2023_NthuIr.page,
-        y2023_NthuOu.page,
-        y2022_Songla.page,
+        y2024_SolwenAi,
+        y2023_Tiic,
+        y2023_NthuIr,
+        y2023_NthuOu,
+        y2022_Songla,
     ]
 )
--- app/builder/projects.py
+++ app/builder/projects.py
@@ -1,39 +1,36 @@
 from app.builder.basic import WebBuilder
-from app.pages.project import (
-    digitalOcean,
-    foodome,
-    hsCloudMeetingManage,
-    hsVmi,
-    jobAnalytics2020,
-    mindReader,
-    noDrinkNoDrunk,
-    oneDayLover,
-    pincakeAi,
-    songla,
-    stockCalc,
-    techlife,
-    trelloFinder,
-    chenmko,
-)
+from app.pages.project.trelloFinder import page as trelloFinder
+from app.pages.project.chenmko import page as chenmko
+from app.pages.project.songla import page as songla
+from app.pages.project.mindReader import page as mindReader
+from app.pages.project.hsCloudMeetingManage import page as hsCloudMeetingManage
+from app.pages.project.hsVmi import page as hsVmi
+from app.pages.project.pincakeAi import page as pincakeAi
+from app.pages.project.stockCalc import page as stockCalc
+from app.pages.project.foodome import page as foodome
+from app.pages.project.noDrinkNoDrunk import page as noDrinkNoDrunk
+from app.pages.project.oneDayLover import page as oneDayLover
+from app.pages.project.jobAnalytics2020 import page as jobAnalytics2020
+from app.pages.project.digitalOcean import page as digitalOcean
+from app.pages.project.techlife import page as techlife
 
 projectBuilder = WebBuilder("projects/")
 
-
 projectBuilder.addPages(
     [
-        trelloFinder.page,
-        chenmko.page,
-        songla.page,
-        mindReader.page,
-        hsCloudMeetingManage.page,
-        hsVmi.page,
-        pincakeAi.page,
-        stockCalc.page,
-        foodome.page,
-        noDrinkNoDrunk.page,
-        oneDayLover.page,
-        jobAnalytics2020.page,
-        digitalOcean.page,
-        techlife.page,
+        trelloFinder,
+        chenmko,
+        songla,
+        mindReader,
+        hsCloudMeetingManage,
+        hsVmi,
+        pincakeAi,
+        stockCalc,
+        foodome,
+        noDrinkNoDrunk,
+        oneDayLover,
+        jobAnalytics2020,
+        digitalOcean,
+        techlife,
     ]
 )
