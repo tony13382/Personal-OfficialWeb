22152c378b6537dce968aed9d34fbf91a4548184
tony13382 <ttsmcpe@gmail.com>
2025-05-07 23:58:07 +0800

    新增獎項資料夾的建立邏輯，並調整專案資料夾的建立順序

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from app.builder.jobs import jobBuilder
 from app.builder.projects import projectBuilder
 from app.builder.skill import skillBuilder
@@ -6,9 +7,12 @@ import app.builder.awards as awardsBuilder
 
 import app.builder.home as homeBuilder
 
+
 projectBuilder.build()
 jobBuilder.build()
 skillBuilder.build()
+os.makedirs("projects", exist_ok=True)
 projects_homeBuilder.build()
+os.makedirs("awards", exist_ok=True)
 awardsBuilder.build()
 homeBuilder.build()
