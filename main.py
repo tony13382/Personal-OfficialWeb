e1d47da007e907b7dcd501a6f0e21a77cb2c2041
tony13382 <ttsmcpe@gmail.com>
2025-10-28 21:43:04 +0800

    Auto generate sitemap.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app.builder.projects import projectBuilder
 from app.builder.skill import skillBuilder
 import app.builder.projects_index as projects_homeBuilder
 import app.builder.awards as awardsBuilder
+from app.builder.sitemap import sitemapBuilder
 
 import app.builder.home_index as homeBuilder
 
@@ -14,3 +15,6 @@ skillBuilder.build()
 projects_homeBuilder.build()
 awardsBuilder.build()
 homeBuilder.build()
+
+# 最後生成 sitemap
+sitemapBuilder.build()
