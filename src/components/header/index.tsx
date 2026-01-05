dcadde71b3ba907e3ec3964e173cd94384edb766
tony13382 <ttsmcpe@gmail.com>
2026-01-06 00:31:15 +0800

    fix: 修正「工作經歷」標籤以符合內容，優化獎項和工作頁面的佈局

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -10,5 +10,5 @@ export interface NavLink {
 export const mainLinks: NavLink[] = [
     // { href: '/projects/', label: '所有作品', icon: <LaptopIcon className='size-8 me-2' /> },
     { href: '/awards/', label: '得獎紀錄', icon: <MedalIcon className='size-8 me-2' /> },
-    { href: '/jobs/', label: '經驗', icon: <BriefcaseIcon className='size-8 me-2' /> },
+    { href: '/jobs/', label: '工作經歷', icon: <BriefcaseIcon className='size-8 me-2' /> },
 ]
